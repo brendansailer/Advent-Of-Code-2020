de7156ae248e9fbd0033b8ae0479867befa01efc
Brendan Sailer <bsailer1@nd.edu>
2020-12-09 23:23:00 -0800

    day10: update solution to part 2

--- day10/program2.py
+++ day10/program2.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 
 def read_input(file_name):
-    return sorted([int(line.strip()) for line in open(file_name, 'r')], reverse=True)
+    return sorted([int(line.strip()) for line in open(file_name, 'r')])
 
-def configure_adaptors(index, adaptors):
+def configure_adaptors(adaptors):
     dp = [0]*len(adaptors)
     dp[0] = 1
     
     for i in range(len(adaptors)):
         for j in range(i+1, i+4):
-            if j < len(adaptors) and adaptors[i] - adaptors[j] <= 3:
+            if j < len(adaptors) and adaptors[j] - adaptors[i] <= 3:
                 dp[j] += dp[i]
 
     return dp[-1]
 
 def main():
-    adaptors = read_input('input2.txt')
-    count = configure_adaptors(0, [adaptors[0] + 3] + adaptors + [0])
+    adaptors = read_input('input.txt')
+    count = configure_adaptors([0] + adaptors + [adaptors[-1] + 3])
     print(count)
 
 if __name__ == '__main__':
